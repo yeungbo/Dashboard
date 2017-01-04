c8005f18c905650af1dad40497f090b87fdb28a7
yangbo <yangbo@ADMINIB-GOOHBDF.crl.ibm.com>
2017-01-04 21:30:52 +0800

    usage line

--- C-dashboard/www/js/controllers.js
+++ C-dashboard/www/js/controllers.js
@@ -95,9 +95,9 @@ angular.module('starter.controllers', [])
 	      var db2share = (db2num/totalnum)*100;
 	      var wasshare = (wasnum/totalnum)*100;
 	      var mongodbshare = (mongoDBnum/totalnum)*100;
-	      console.log("db2:"+db2share)
-	      console.log("mongodbshare:"+mongodbshare)
-	      console.log("was:"+wasshare)
+//	      console.log("db2:"+db2share)
+//	      console.log("mongodbshare:"+mongodbshare)
+//	      console.log("was:"+wasshare)
 	      
 	      
 //	      alert("defer"+ JSON.stringify(defer) );
@@ -131,7 +131,7 @@ angular.module('starter.controllers', [])
 	             },
 	             series: [{
 	                 type: 'pie',
-	                 name: 'Browser share',
+	                 name: 'Resource share',
 	                 data: [
 	                     ['DB2',   db2share],
 	                     ['WAS',       wasshare],
@@ -258,54 +258,267 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope, Accounts) {
+.controller('AccountCtrl', function($scope,  $http, Accounts) {
 //	alert("AccountCtrl");
 	
+	var msgdata ="abc";
+	console.log(msgdata);
+//	var defer = $q.defer();
+	 $http({
+         method: 'GET',
+         url: 'http://cap-sg-prd-4.integration.ibmcloud.com:16763/mfp/api/adapters/javaAdapter/resource/vlanhealth'
+      }).success(function(data) {
 	
+	
+//	defer.resolve(data);
+//	$obj1 = angular.toJson(data); 
+	$msgdata=angular.toJson(data); 
+	
+//    console.log("success"+$msgdata);
+    
+    var json = JSON.parse($msgdata); 
+    
+    var subnets = [];
+    for(var key in json){  
+//		console.log(key);  
+//		console.log(json[key]); 
+		var jsonobj=json[key];
+		for (var k in jsonobj)
+			{
+			
+//			console.log(k);  
+//			console.log(jsonobj[k]); 
+			subnets = jsonobj["subnets"];
+			}
+    } 
+    
+//    for(var s in subnets){ 
+////    	console.log(s); 
+////    	console.log(subnets[s]); 
+//    	
+//    	var sn=subnets[s];
+//    	console.log(sn["subnet"]); 
+//    	console.log(sn["usage"]);
+//    	console.log(sn["capacity"]);
+//    	
+//    }
+    
+//  alert("success"+data);
+//  alert("success"+data.was);
+  
+  
+  
+//  alert("defer"+ JSON.stringify(defer) );
+  
+  ///////////////////////////
+    $scope.vlanPieConfig = {  
+    		chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Usage for VLAN 1586'
+            },
+            subtitle: {
+                text: 'Source: <a href="https://en.wikipedia.org">WHC Platform</a>'
+            },
+            xAxis: {
+                categories: (function () {
+	                    // generate an array of random data
+	                    var data = [];
+	                    
+	                    for(var s in subnets){ 
+	                    	var sn=subnets[s];
+		                    data.push(sn["subnet"]);
+	                    }
+	                    var $jdata=angular.toJson(data); 
+	                    console.log(data);
+	                    console.log($jdata);
+	                    return data;
+	                }()),
+                	
+                	
+//                	['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'IP numbers',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' IP'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 80,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: (function () {
+                // generate an array of random data
+                var data = [];
+                var cap = [];
+                var usa = [];
+                for(var s in subnets){ 
+                	var sn=subnets[s];
+                    cap.push(sn["capacity"]);
+                    usa.push(sn["usage"]);
+                }
+                
+                data.push({
+                	name: "Capacity",
+                	data: cap
+                	});
+                
+                data.push({
+                	name: "Usage",
+                	data: usa
+                	});
+                
+               
+                var $jdata=angular.toJson(data); 
+                console.log(data);
+                console.log($jdata);
+                return data;
+            }())
+            	
+            	
+            	
+//            	[{
+//                name: 'Year 1800',
+//                data: [107, 31, 635, 203, 2]
+//            }, {
+//                name: 'Year 1900',
+//                data: [133, 156, 947, 408, 6]
+//            }, {
+//                name: 'Year 2012',
+//                data: [1052, 954, 4250, 740, 38]
+//            }]
+    	    };
+    //////////////////////////////////////
+//  //饼图
+//  $scope.chartPieConfig = {
+//
+//   chart: {
+//             plotBackgroundColor: null,
+//             plotBorderWidth: null,
+//             plotShadow: false
+//         },
+//         title: {
+//             text: 'Browser resource usage at a specific environment, 2016'
+//         },
+//         tooltip: {
+//          pointFormat: '{series.name}bb: <b>{point.percentage:2.2f}%</b>'
+//         },
+//         plotOptions: {
+//             pie: {
+//                 allowPointSelect: true,
+//                 cursor: 'pointer',
+//                 dataLabels: {
+//                     enabled: true,
+//                     color: '#000000',
+//                     connectorColor: '#000000',
+//                     format: '<b>{point.name}cc</b>: {point.percentage:1.1f} %'
+//                 }
+//             }
+//         },
+//         series: [{
+//             type: 'pie',
+//             name: 'Resource share',
+//             data: [
+//                 ['DB2',   db2share],
+//                 ['WAS',       wasshare],
+//                 {
+//                     name: 'MongoDB',
+//                     y: mongodbshare,
+//                     sliced: true,
+//                     selected: true
+//                 }
+//             ]
+//         }]
+//  };
+  /////////////////////////////
+  
+  return $msgdata;
+}).error(function(data, status, headers, config){	
+	defer.reject(data);
+  alert("登录出错" + data + "  " + status);
+});
+	///////////////////////////
+	   
+	 
+	 
+	 ////////////////////
 	  //饼图
-	 $scope.vlanPieConfig = {
+//	 $scope.vlanPieConfig = {
+//	
+//	  chart: {
+//	            plotBackgroundColor: null,
+//	            plotBorderWidth: null,
+//	            plotShadow: false
+//	        },
+//	        title: {
+//	            text: 'Browser resource usage at a specific environment, 2016'
+//	        },
+//	        tooltip: {
+//	         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+//	        },
+//	        plotOptions: {
+//	            pie: {
+//	                allowPointSelect: true,
+//	                cursor: 'pointer',
+//	                dataLabels: {
+//	                    enabled: true,
+//	                    color: '#000000',
+//	                    connectorColor: '#000000',
+//	                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+//	                }
+//	            }
+//	        },
+//	        series: [{
+//	            type: 'pie',
+//	            name: 'Browser share',
+//	            data: [
+//	                ['Firefox',   45.0],
+//	                ['IE',       26.8],
+//	                {
+//	                    name: 'Chrome',
+//	                    y: 12.8,
+//	                    sliced: true,
+//	                    selected: true
+//	                },
+//	                ['Safari',    8.5],
+//	                ['Opera',     6.2],
+//	                ['Others',   0.7]
+//	            ]
+//	        }]
+//	 };
 	
-	  chart: {
-	            plotBackgroundColor: null,
-	            plotBorderWidth: null,
-	            plotShadow: false
-	        },
-	        title: {
-	            text: 'Browser resource usage at a specific environment, 2016'
-	        },
-	        tooltip: {
-	         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-	        },
-	        plotOptions: {
-	            pie: {
-	                allowPointSelect: true,
-	                cursor: 'pointer',
-	                dataLabels: {
-	                    enabled: true,
-	                    color: '#000000',
-	                    connectorColor: '#000000',
-	                    format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-	                }
-	            }
-	        },
-	        series: [{
-	            type: 'pie',
-	            name: 'Browser share',
-	            data: [
-	                ['Firefox',   45.0],
-	                ['IE',       26.8],
-	                {
-	                    name: 'Chrome',
-	                    y: 12.8,
-	                    sliced: true,
-	                    selected: true
-	                },
-	                ['Safari',    8.5],
-	                ['Opera',     6.2],
-	                ['Others',   0.7]
-	            ]
-	        }]
-	 };
+	
+	
+	////////////////////////////////////////////
 //	  Accounts.all($http).success(function(data) {
 //		  	$scope.accounts = data
 //		  	alert("ii:"+$scope.accounts);
